569ee1ddd2e8f0534adaa2bb4c24bbc57140c6ae
SamaPozitiff <sama.pozitiff@gmail.com>
2022-10-24 10:31:55 +0300

    Старт проекта

---
+++ src/main/java/com/example/homepagenewspaper/HomePageNewsPaperApplication.java
@@ -0,0 +1,13 @@
+package com.example.homepagenewspaper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomePageNewsPaperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomePageNewsPaperApplication.class, args);
+    }
+
+}
