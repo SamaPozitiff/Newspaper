e5a5ea4079b240ede79b8968f3f2942c96a370af
SamaPozitiff <sama.pozitiff@gmail.com>
2022-10-31 10:35:00 +0300

    интерфейс-репозиторий для статьи

---
+++ src/main/java/Repositories/NewspaperArticleRepository.java
@@ -0,0 +1,11 @@
+package Repositories;
+
+
+import Entities.NewspaperArticleEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NewspaperArticleRepository extends JpaRepository<NewspaperArticleEntity, Long> {
+
+}
