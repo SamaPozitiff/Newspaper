765b20e44b4cad87d0fbf6543f14b304595527e1
SamaPozitiff <sama.pozitiff@gmail.com>
2022-10-31 10:33:18 +0300

    доработан класс-сущность для статьи

---
+++ src/main/java/Entities/NewspaperArticleEntity.java
@@ -0,0 +1,47 @@
+package Entities;
+
+
+import javax.persistence.*;
+import javax.swing.*;
+import java.awt.*;
+import java.util.*;
+import java.util.List;
+
+
+@Entity
+@Table(name = "articles")
+public class NewspaperArticleEntity {
+    @Id
+    @GeneratedValue
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "title")
+    private String title;
+    @Column(name = "image")
+    private ImageIcon image;
+    @Column(name = "description")
+    private String description;
+    @ManyToOne
+    @JoinColumn(name = "author")
+    private UserEntity author;
+    @Column (name = "date")
+    private Date publicationDate;
+    @OneToMany
+    private List<CommentEntity> comments;
+    private int likes;
+
+    public NewspaperArticleEntity(){
+
+    }
+
+    public NewspaperArticleEntity(Long id, String title, ImageIcon image, String description, UserEntity author) {
+        this.id = id;
+        this.title = title;
+        this.image = image;
+        this.description = description;
+        this.author = author;
+        this.publicationDate = new Date();
+        this.comments = new ArrayList<>();
+        this.likes = 0;
+    }
+}
