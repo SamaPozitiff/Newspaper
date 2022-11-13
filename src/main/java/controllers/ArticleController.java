79ce5a17c53ea7eb027d8a8df63fcbe483368c94
SamaPozitiff <sama.pozitiff@gmail.com>
2022-11-13 19:35:56 +0300

    удаление и создание статей в контроллере

--- src/main/java/controllers/ArticleController.java
+++ src/main/java/controllers/ArticleController.java
@@ -2,30 +2,46 @@ package controllers;
 
 
 import entities.ArticleEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import services.ArticleService;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import services.CommentService;
+import services.UserService;
 
 
 import java.util.List;
 
 @RestController
 public class ArticleController {
-    private final ArticleService service;
+    private final ArticleService articleService;
+    private final UserService userService;
 
-    public ArticleController(ArticleService service){
-        this.service = service;
+    public ArticleController(ArticleService articleService, UserService userService){
+        this.articleService = articleService;
+        this.userService = userService;
     }
 
     @GetMapping(path = "/articles" ,produces = MediaType.APPLICATION_JSON_VALUE)
     public List<ArticleEntity> getAllArticlesFor24Hours(){
-        return service.getAllArticlesFor24Hours();
+        return articleService.getAllArticlesFor24Hours();
     }
 
     @GetMapping(path = "/last_articles",produces = MediaType.APPLICATION_JSON_VALUE)
     public List<ArticleEntity> get3LastArticles(){
-        return service.get3LastArticles();
+        return articleService.get3LastArticles();
+    }
+
+    @PostMapping(path = "/articles/add")
+    public ResponseEntity<ArticleEntity> addArticle(@RequestParam String title, String description, Long userId){
+       return new ResponseEntity<>(articleService.save(new ArticleEntity(title, null, description, userService.findById(userId))), HttpStatus.CREATED);
+
+    }
+
+    @DeleteMapping(path = "/articles/delete")
+    public void delete(@RequestParam Long articleId){
+        articleService.delete(articleId);
     }
 
 }
--- src/main/java/services/ArticleService.java
+++ src/main/java/services/ArticleService.java
@@ -10,10 +10,12 @@ import java.util.List;
 @Service
 public class ArticleService {
     private final ArticleRepository repository;
+    private final CommentService commentService;
 
 
-    public ArticleService(ArticleRepository repository){
+    public ArticleService(ArticleRepository repository, CommentService commentService){
         this.repository = repository;
+        this.commentService = commentService;
     }
 
     public List<ArticleEntity> getAllArticlesFor24Hours(){
@@ -24,11 +26,19 @@ public class ArticleService {
         return repository.findLast3Articles();
     }
 
-    public void save(ArticleEntity entity){
-        repository.save(entity);
+    public ArticleEntity save(ArticleEntity entity){
+        return repository.save(entity);
     }
 
     public ArticleEntity findById(Long id){
         return repository.findById(id).get();
+    }
+
+    public void delete (Long id){
+        List<CommentEntity> comments= commentService.findAllCommentsOfArticle(id);
+        for (CommentEntity comment:comments){
+            commentService.delete(comment.getId());
+        }
+        repository.deleteById(id);
     }
 }
